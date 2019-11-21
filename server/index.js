8ea00e8ad00df27bfd225e5e5b34b1bbe4cc3c0b
Aysa Grabert <aysagrabert@Aysas-MacBook-Pro.local>
2019-11-21 11:21:33 -0700

    finished

--- server/index.js
+++ server/index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const getProducts = require('./getProducts');
+const getProducts = require('./getProduct');
 
 const app = express();
 
 const port = 6660;
 
 app.get('/api/products', getProducts);
+app.get('/api/product/:id', getProduct);
 
 app.listen(port, () => {
-    console.log(`${port} is Haunted!`)
+    console.log(`Running on port: ${port}☠️`);
 });
